9589b7a7f31ed9811a53e02cdc045bc4deff345f
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2023-02-03 00:37:57 +0100

    problem solving

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from 'react';
 import Form from './Components/Form/Form';
 
 export default function App() {
+  useEffect(() => {
+    document.body.style.minHeight = `${window.innerHeight}px`;
+    return () => {
+      document.body.style.minHeight = '';
+    };
+  }, []);
+
   return <Form />;
 }
